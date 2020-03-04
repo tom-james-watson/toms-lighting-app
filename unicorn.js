918857b25676cbec10bf5e8867d1487d4522c5a5
Tom Watson <tom@tomjwatson.com>
2020-03-04 10:29:17 +0100

    update lighting ip address

--- unicorn.js
+++ unicorn.js
@@ -2,13 +2,13 @@ function setColor(color) {
     color = color.substr(1, 6);
 
     let xhr = new XMLHttpRequest();
-    xhr.open('GET', 'http://192.168.0.163:5000?color=' + color, true);
+    xhr.open('GET', 'http://192.168.1.52:5000?color=' + color, true);
     xhr.send();
 }
 
 function turnOff() {
     let xhr = new XMLHttpRequest();
-    xhr.open('GET', 'http://192.168.0.163:5000/off', true);
+    xhr.open('GET', 'http://192.168.1.52:5000/off', true);
     xhr.send();
 }
 
